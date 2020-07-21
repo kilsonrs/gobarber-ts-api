0fbce157123affde2d58bd8f1720c2704bea1f2a
Kilson <kilsonrs@gmail.com>
2020-07-21 08:38:43 -0300

    Upload files

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
-const usersRouter = Router();
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
+const usersRouter = Router();
+const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
@@ -19,5 +23,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
