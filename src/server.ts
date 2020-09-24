8cd511e6b483b84ecbb3921c993cac21ce7ed6c4
Kilson <kilsonrs@gmail.com>
2020-09-24 10:08:45 -0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -7,6 +8,7 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -28,5 +30,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  console.log('🚀 Server started on port 3333');
 });
