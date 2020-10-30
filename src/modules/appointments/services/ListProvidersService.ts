4e122f2118f2453d66753497cb69a2530dca782b
Kilson <kilsonrs@gmail.com>
2020-10-30 14:38:08 -0300

    Cache lista de provedores

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,7 @@
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 import { injectable, inject } from 'tsyringe';
 
 interface IRequest {
@@ -12,10 +14,23 @@ class ListProvidersService {
     @inject('UsersRepository')
     private usersRepository: IUserRepository,
 
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({except_user_id: user_id});
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    }
+
     return users;
   }
 }
